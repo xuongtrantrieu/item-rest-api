dd4c94074522dbbdc50ca77f724d046a6e96ad82
Xuong Tran Trieu <xuong.trantrieu@gmail.com>
2018-03-13 12:04:40 +0700

    Implement PosgresSQL to our API

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +10,8 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # Remember
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # Remember
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Remember
 app.secret_key = 'QQ'
 api = Api(app)
